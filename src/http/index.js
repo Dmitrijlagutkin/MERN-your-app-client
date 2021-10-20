c02e76e8aa3c593693b904066623a475a3b3acfc
Dmitrijlagutkin <dmitrijlagutkin@gmail.com>
2021-10-20 16:17:16 +0300

    smal changes

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_URL = "http://localhost:5000/api"
+export const API_URL = "https://your-list-app.herokuapp.com/api"
 // "http://localhost:5000/api"
 // "https://your-list-app.herokuapp.com/api"
 
